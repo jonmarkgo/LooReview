19dc6f696af1155d991d007a44b09d4347422487
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-09-18 09:06:09 -0400

    maybe I'm a dumbass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,14 @@ class SessionsController < ApplicationController
     @access_token = foursquare.access_token(code, callback_session_url)
    
   session[:access_token] = @access_token
-  #  if (current_user)
-   #   current_user.access_token = @access_token
-    #  puts @access_token
-    #  current_user.save
-    #end
+    if (current_user)
+      @puser = User.find_by_uid(current_user.id)
+      @puser.access_token = @access_token
+      puts @access_token
+      puts "eheh"
+      puts @puser.to_json
+      @puser.save
+    end
     redirect_to examples_path
   end
   
