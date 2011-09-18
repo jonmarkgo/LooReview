1b0ef09c08486897d83f4df62a09917d981b9cde
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-09-17 20:25:06 -0400

    try array'

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -29,7 +29,7 @@ class FoursquareController < ApplicationController
 def lol
 @photoless = Toilet.where('photo_url = \'meh\'')
 		@photoless.each do | pltoilet |
-			@checkin = foursquare.checkins.find(pltoilet[:checkin_id])
+			@checkin = foursquare.checkins.find(pltoilet['checkin_id'])
 			pltoilet.photo_url = @checkin['photos']['items'].first['url']
 			pltoilet.save
 			puts pltoilet
