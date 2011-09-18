7b5653acad2ef0785a4c3e64d3fbf4bf821aed86
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-09-18 10:52:15 -0400

    Country?

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,11 +1,5 @@
 class Toilet < ActiveRecord::Base
 	belongs_to :user
-	reverse_geocoded_by :venue_lat, :venue_lng do |obj,results|
-  if geo = results.first
-    obj.city    = geo.city
-    obj.zipcode = geo.postal_code
-    obj.country = geo.country_code
-  end
-end
+reverse_geocoded_by :venue_lat, :venue_lng
 after_validation :reverse_geocode 
 end
