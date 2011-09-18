da9d48d54db56a51e49071ae85e171ef758cb47a
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-09-17 20:32:04 -0400

    debug

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -29,12 +29,12 @@ class FoursquareController < ApplicationController
 def lol
 @photoless = Toilet.where('photo_url = \'meh\'')
 		@photoless.each do | pltoilet |
-			@myt = pltoilet[:toilet]
+			@myt = pltoilet.toilet
 puts @myt.to_json
 			@checkin = foursquare.checkins.find(@myt['checkin_id'])
 			puts @checkin.to_json
-		pltoilet.photo_url = @checkin['photos']['items'].first['url']
-		pltoilet.save
+		#pltoilet.photo_url = @checkin['photos']['items'].first['url']
+		#pltoilet.save
 			#puts pltoilet
 		end
 end
