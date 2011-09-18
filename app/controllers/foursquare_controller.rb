df60ed3fbaa72dc556eb4f1507e25b3c1626f82f
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-09-18 09:24:50 -0400

    Render status OK

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -6,18 +6,6 @@ class FoursquareController < ApplicationController
 		@shouttxt = @checkin['shout']
 		@venue = @checkin['venue']
 
-#@photoless = Toilet.where('photo_url = \'meh\'')
-
-#		@photoless.each do | pltoilet |
-		#	@puser = pltoilet.user
-
-	#	pltoilet.photo_url = @checkin.json['photos']['items'].at(1)['url']
-#	@meh = @checkin.json['photos']['items'].first
-#	@meh = @meh['sizes']['items'].at(1)
-#	pltoilet.photo_url = @meh['url']
-#		pltoilet.save
-	#	puts pltoilet
-		#end
 		if (@shouttxt.index('toilet') != nil)
 			@toilet = Toilet.new
 			@toilet.user_id = @checkin['user']['id']
@@ -29,24 +17,19 @@ class FoursquareController < ApplicationController
 			@toilet.venue_name = @venue['name']
 			@toilet.venue_id = @venue['id']
 			@puser = User.find_by_uid(@checkin['user']['id'])
-			puts "0"
-			puts @puser.to_json
+		
 						foursquare = Foursquare::Base.new(@puser['token'])
-						puts"1"
-						puts foursquare
-						puts "2"
-						puts foursquare.to_json
+				
 			@checkedin = foursquare.checkins.find(@checkin['id'])
 		@meh = @checkedin.json['photos']['items'].first
 	@meh = @meh['sizes']['items'].at(1)
 	@toilet.photo_url = @meh['url']
-	puts "3"
-	puts @meh.to_json
-	puts "toilet"
-	puts @toilet.to_json
+
 
 			@toilet.save
+			
 		end
+			render :status => :ok
 	end
 
 end
