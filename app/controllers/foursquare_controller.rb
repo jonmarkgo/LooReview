74fb91bc36d3da35167d1de8d0753aed2eddf612
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-09-17 22:16:57 -0400

    TEST

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -5,15 +5,13 @@ class FoursquareController < ApplicationController
 		@checkin = ActiveSupport::JSON.decode(params[:checkin])
 		@shouttxt = @checkin['shout']
 		@venue = @checkin['venue']
-@photoless = Toilet.where('photo_url = \'meh\'')
-		@photoless.each do | pltoilet |
-	@checkin = foursquare.checkins.find(pltoilet[:checkin_id])
-		pltoilet.photo_url = @checkin.json['photos']['items'].at(1)
-		puts pltoilet.to_json
-		pltoilet.photo_url = pltoilet.photo_url['url']
-		pltoilet.save
-		puts pltoilet
-		end
+#@photoless = Toilet.where('photo_url = \'meh\'')
+#		@photoless.each do | pltoilet |
+	#@checkin = foursquare.checkins.find(pltoilet[:checkin_id])
+	#	pltoilet.photo_url = @checkin.json['photos']['items'].at(1)['url']
+#		pltoilet.save
+#		puts pltoilet
+#		end
 		if (@shouttxt.index('toilet') != nil)
 			@toilet = Toilet.new
 			@toilet.user_id = @checkin['user']['id']
@@ -31,9 +29,10 @@ def lol
 	@photoless = Toilet.where('photo_url = \'meh\'')
 		@photoless.each do | pltoilet |
 	@checkin = foursquare.checkins.find(pltoilet[:checkin_id])
-		pltoilet.photo_url = @checkin.json['photos']['items'].at(1)['url']
-		pltoilet.save
-		puts pltoilet
+	puts @checkin.json['photos']['items'].to_json
+		#pltoilet.photo_url = @checkin.json['photos']['items'].at(1)['url']
+		#pltoilet.save
+		#puts pltoilet
 		end
 end
 end
